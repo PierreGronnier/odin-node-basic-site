e58db296c4c91989ae38e3106c2b28f6911a3a47
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-09-09 15:40:28 +0200

    add the express version in comment

--- index.js
+++ index.js
@@ -65,3 +65,32 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/* Version Express :
+
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, "public", req.path + ".html");
+  if (fs.existsSync(filePath)) {
+    return res.sendFile(filePath);
+  }
+  next(); 
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+*/
